e522e0395b4610208fac972737230a0da39c1aae
uc02 <upendrachauhan0206@gmail.com>
2025-04-15 21:08:53 +0530

    second commit

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,24 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { descriptions, images } from '../data'
+import { ArrowLeft, ArrowRight } from 'lucide-react'
  
 const Slider = () => {
+
+  const [index, setIndex] = useState(3)
+
   return (
     //container
-    <div>
+    <div className='relative'>
       {/* Slider */}
-      <div>
+      <div className='flex gap-x-20 lg:items-start items-center lg:flex-row flex-col'>
+
         {/*  Image */}
         <div className='sm:w-[400px] sm:h-[400px] w-[300px] h-[300px] relative'>
            {images.map((image,i) => (
-            <img key={i} src={image} className='w-full h-full absolute object-cover rounded-3xl' />
+            <img 
+            key={i} 
+            src={image} 
+            className={`w-full h-full absolute object-cover rounded-3xl ${i === index ? 'activeImage' : 'inactiveImage'}`} />
            ))}
         </div>
 
         {/* descriptions */}
-        <div>{descriptions.map((desc,i) => (
-          <p key={i}>{desc}</p>
+        <div className='relative sm:w-[400px] w-[320px] mt-22 lg:mt-5'>
+          {descriptions.map((desc,i) => (
+          <p className={`text-center sm:text-xl text-gray-600 absolute 
+            ${ i === index ? 'activeDesc' : 'inactiveDesc'}`}
+           key={i}>          
+            {desc}
+            </p>
         ))}
         </div>
+      </div>
+
+      {/* Controls */}
+      <div className='absolute bottom-0 lg:-bottom-20 left-1/2 translate-1/2 flex gap-x-5'>
+        <button className='bg-gray-100 p-1.5 cursor-pointer rounded-full text-gray-600 hover:br-gray-200 transition-colors' onClick={() => setIndex((prev) => (prev === 0 ? images.length - 1 : prev - 1))} >
+          <ArrowLeft  size={18} />
+        </button>
+        <button className='bg-gray-100 p-1.5 cursor-pointer rounded-full text-gray-600 hover:br-gray-200 transition-colors' onClick={() => setIndex((prev) => (prev === images.length - 1 ? 0 : prev + 1))}>
+          <ArrowRight  size={18} />
+        </button>
       </div>
     </div>
   )
